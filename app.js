c580f9d69bd277a47e8c6ab0d8cb3f4bbfa48dce
Mustafain Ali Khan <mustafain.khan@mail.mcgill.ca>
2020-01-01 21:26:05 -0500

    income and expense constructors

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 var budgetCalc = (function() {
+	
+	var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalInc) {
+    
+        if(totalInc > 0){
+          this.percentage = Math.round((this.value / totalInc)*100);
+        }
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
+    };
+    
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
 })();
 
